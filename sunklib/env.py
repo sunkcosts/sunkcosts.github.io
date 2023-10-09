2565c6fb44914b862c19698a228f28bf6b592005
Hart Traveller <hart@PRHTMBA15.local>
2023-10-09 13:45:55 -0700

    sunklib/env: UPDATE

--- sunklib/env.py
+++ sunklib/env.py
@@ -1,5 +1,15 @@
 from pathlib import Path
 from importlib import metadata
+from sunklib.util.io import save_toml
 
 
 VERSION = metadata.version("sunklib")
+
+PATH_HOME = Path.home()
+PATH_PACKAGE = PATH_HOME / ".sunklib"
+FILE_CREDENTIALS = PATH_PACKAGE / "credentials.toml"
+
+if not PATH_PACKAGE.exists():
+    PATH_PACKAGE.mkdir(exist_ok=True)
+if not FILE_CREDENTIALS.exists():
+    save_toml(dict(), FILE_CREDENTIALS)
