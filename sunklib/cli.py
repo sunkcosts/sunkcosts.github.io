b93a0aaa69f435fcc6359a6a5290d8588da18112
Hart Traveller <hart@PRHTMBA15.local>
2023-10-09 15:39:08 -0700

    sunklib/cli: update

--- sunklib/cli.py
+++ sunklib/cli.py
@@ -1,7 +1,8 @@
 import click
+import subprocess
 from sunklib.util.auth import credentials
 from sunklib.util.term import vprint
-from sunklib.env import VERSION
+from sunklib.env import VERSION, APP_ENTRY_POINT
 
 
 @click.group()
@@ -9,6 +10,9 @@ from sunklib.env import VERSION
 @click.pass_context
 def scli(ctx):
     pass
+
+
+# ! ---
 
 
 @scli.group()
@@ -50,4 +54,71 @@ def delete(name: str) -> None:
 def copy():
     "copy key to clipboard"
     # TODO
+    raise NotImplementedError()
+
+
+# ! ---
+
+
+@scli.group()
+def model():
+    "Model utilities."
+    pass
+
+
+@model.command(name="gui")
+@click.option(
+    "--browser/--no-browser",
+    type=bool,
+    default=False,
+    help="Automatically open the browser, defaults to false.",
+)
+@click.option(
+    "--run-on-save/--no-run-on-save",
+    type=bool,
+    default=True,
+    help="Rerun the app when file saved, defaults to false.",
+)
+@click.option(
+    "--host",
+    type=str,
+    default="localhost",
+    help="Host path to serve app on, defaults to localhost.",
+)
+@click.option(
+    "--port",
+    type=int,
+    default=4096,
+    help="Port to serve app on, defaults to 4096.",
+)
+def model_gui(browser: bool, run_on_save: bool, host: str, port: int):
+    """Start the model GUI."""
+    subprocess.run(
+        [
+            "streamlit",
+            "run",
+            APP_ENTRY_POINT,
+            "--server.headless",
+            "false" if browser else "true",
+            "--logger.enableRich",
+            "true",
+            "--browser.gatherUsageStats",
+            "false",
+            "--server.runOnSave",
+            "true" if run_on_save else "false",
+            "--server.address",
+            host,
+            "--server.port",
+            str(int(port)),
+            "--theme.primaryColor",
+            "#2671c7",
+            "--theme.base",
+            "dark",
+        ]
+    )
+
+
+@model.command(name="forecast")
+def model_forecast():
+    "Not yet implemented, forecast economic cost on terminal given input parameters."
     raise NotImplementedError()
