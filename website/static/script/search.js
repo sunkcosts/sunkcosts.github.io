4caf47368840c0d5244eda3052bf61cbaf9e65b3
Hart Traveller <hart@MBP.local>
2023-10-15 14:39:27 -0400

    website/static/script/search: update search

--- website/static/script/search.js
+++ website/static/script/search.js
@@ -1,19 +1,38 @@
-
-document.addEventListener('DOMContentLoaded', function () {
-    setInterval(manage_search, 100);
-})
-
-function search_expanded() {
-    var focus_visible = document.querySelectorAll(".md-search__input.focus-visible");
-    return focus_visible.length > 0;
+function openSearch() {
+    search_input.focus()
+    search_input.select()
+    header.style["z-index"] = "10000";
+    search.style["opacity"] = "1";
+    quick_search_open = true;
+    console.log("open search")
 }
 
-function manage_search() {
-    var header = document.querySelector('.md-header')
-    var drawer_icon = document.querySelector('.md-header__button.md-icon[for="__drawer"]')
-    if (search_expanded()) {
-        header.style["z-index"] = "11";
-    } else {
-        drawer_icon.style["z-index"] = "0";
+function closeSearch() {
+    header.style["z-index"] = "0";
+    search.style["opacity"] = "0";
+    search_input.blur()
+    quick_search_open = false;
+    console.log("close search")
+
+}
+
+function checkShortcuts(event) {
+    if (((event.metaKey || event.ctrlKey) && event.key === 'k') && (!quick_search_open)) {
+        openSearch()
+    } else if ((((event.metaKey || event.ctrlKey) && event.key === 'k') || (event.keyCode == 27)) && (quick_search_open)) {
+        closeSearch();
     }
+}
+
+var search = document.querySelector('.md-search')
+var search_input = document.querySelector('.md-search__input')
+var search_overlay = document.querySelector(".md-search__overlay")
+var header = document.querySelector('.md-header')
+
+var quick_search_open = false;
+
+document.onkeydown = checkShortcuts
+
+search_overlay.onclick = function () {
+    closeSearch()
 }
